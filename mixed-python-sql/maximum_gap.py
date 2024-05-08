658b3d45616fa08c17b0ac20bb1b165537c810c1
tinavo <tina.n.vo58@gmail.com>
2024-05-08 22:24:09 +1000

    renamed folder and added files

---
+++ mixed-python-sql/maximum_gap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return 0
+
+        s_nums = sorted(nums)
+        l_nums =  [(s_nums[i], s_nums[i+1]) for i in range(len(s_nums)) if i < (len(s_nums) - 1)]
+
+        return max([abs(a-b) for a, b in l_nums])
+
+
+
+if __name__ == '__main__':
+    nums = [3, 6, 9, 1]
+
+    s = Solution()
+    print(s.maximumGap(nums))
